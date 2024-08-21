62e006633e095d316d8ba42f72a7bc132edf25d0
undefinedCudaCore <teklithuania@gmail.com>
2024-08-21 17:21:43 +0300

    Add project files.

---
+++ GraduationProject/Utilities/FileUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace GraduationProject.Utilities
+{
+    public class FileUtils
+    {
+        public static async Task<byte[]> ConvertToByteArray(IFormFile? file)
+        {
+            using var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}
