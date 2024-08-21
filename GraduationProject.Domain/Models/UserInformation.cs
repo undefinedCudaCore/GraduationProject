6404132c7b63515b9596343d67b6c06cb390e53f
undefinedCudaCore <teklithuania@gmail.com>
2024-08-21 17:37:33 +0300

    Added models

---
+++ GraduationProject.Domain/Models/UserInformation.cs
@@ -0,0 +1,16 @@
+﻿namespace GraduationProject.Domain.Models
+{
+    internal class UserInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PersonalCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string? FileName { get; set; }
+        public byte[]? FileData { get; set; }
+
+        public int UserId { get; set; }
+        public IList<UserResidence> UserResidences { get; set; }
+    }
+}
---
+++ GraduationProject.Domain/Models/UserResidence.cs
@@ -0,0 +1,13 @@
+﻿namespace GraduationProject.Domain.Models
+{
+    internal class UserResidence
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string ApartmentNumber { get; set; }
+
+        public int UserInformationId { get; set; }
+        public UserInformation UserInformation { get; set; }
+    }
+}
